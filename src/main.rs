16809a9f6d4e7edab16f4d8d63ede9b5ed56f517
Dikendev <diken.dev@gmail.com>
2024-01-06 23:41:32 -0300

    bubble sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+fn bubble_sort<T: Ord>(arr: &mut [T]) {
+    if arr.is_empty() {
+        return;
+    }
+    let mut sorted = false;
+    let mut n = arr.len() - 1;
+    println!("format {n} test");
+
+    while !sorted {
+        sorted = true;
+        for i in 0..n {
+            println!(); // prints just a newline
+            println!("{i} index");
+
+            if arr[i] > arr[i + i] {
+                arr.swap(i, i + 1);
+                sorted = false
+            }
+        }
+        n -= 1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello");
+    let mut ve1 = vec![6, 5, 4, 3, 2, 1];
+
+    bubble_sort(&mut ve1);
 }
